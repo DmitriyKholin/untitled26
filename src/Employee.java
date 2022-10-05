a76e2ed64a46519fa1d45bba802fe06efbf77be3
Dmitriy <bcezbs@gmail.com>
2022-10-05 13:39:12 +0300

    homework

--- src/Employee.java
+++ src/Employee.java
@@ -1,5 +1,52 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package com.example.homeworkstreams;
+
+import java.util.Objects;
+
+public class Employee {
+    private  String fullName;
+    private int depId;
+    private  int salary;
+
+    public Employee(String fullName,int depId,int salary){
+        this.fullName=fullName;
+        this.depId=depId;
+        this.salary=salary;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getDepId() {
+        return depId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return fullName.equals(employee.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName);
+    }
+
+    @Override
+    public String toString () {
+        return "Ф.И.О. "+fullName+", отдел - "+depId+", зарплата - "+salary;
     }
 }
--- src/EmployeeController.java
+++ src/EmployeeController.java
@@ -1,2 +1,108 @@
-package PACKAGE_NAME;public class EmployeeController {
+package com.example.homeworkstreams;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+    public EmployeeController ( EmployeeService  employeeService ) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping(path = "/add")
+    public Object addEmployee(
+            @RequestParam(value = "fullName") String fullName,
+            @RequestParam(value = "depId") int depId,
+            @RequestParam(value = "salary") int salary) {
+        Employee employee = null;
+        try {
+            employee = employeeService.addEmployee(fullName, depId, salary);
+        } catch (Throwable e) {
+            return e.getMessage();
+        }
+        return employee;
+    }
+    @GetMapping(path = "/remove")
+    public Object removeEmployee(
+            @RequestParam(value = "fullName") String fullName) {
+        Employee employee = null;
+        try {
+            employee = employeeService.removeEmployee(fullName);
+        } catch (Throwable e) {
+            return e.getMessage();
+        }
+        return employee;
+    }
+    @GetMapping(path = "/find")
+    public Object findEmployee(
+            @RequestParam(value = "fullName") String fullName) {
+        Employee employee = null;
+        try {
+            employee = employeeService.findEmployee(fullName);
+        } catch (Throwable e) {
+            return e.getMessage();
+        }
+        return employee;
+    }
+    @GetMapping(path = "/departament/max-salary")
+    public Object getMaxSalaryEmployee(
+            @RequestParam(value = "departamentId") int depId) {
+        Employee employee = null;
+        try {
+            employee = employeeService.getMaxSalaryEmployee(depId);
+        } catch (Throwable e) {
+            return e.getMessage();
+        }
+        return employee;
+    }
+    @GetMapping(path = "/departament/min-salary")
+    public Object getMinSalaryEmployee(
+            @RequestParam(value = "departamentId") int depId) {
+        Employee employee = null;
+        try {
+            employee = employeeService.getMinSalaryEmployee(depId);
+
+        } catch (Throwable e) {
+            return e.getMessage();
+        }
+        return employee;
+    }
+    @GetMapping(path = "/departament/all-dep")
+    public Object printEmployeesForDepartment(
+            @RequestParam(value = "departamentId") int depId) {
+        List<Employee> employees = null;
+        try {
+            employees = employeeService.printEmployeesForDepartment(depId);
+        } catch (Throwable e) {
+            return e.getMessage();
+        }
+        return employees;
+    }
+
+    @GetMapping(path = "/departament/all")
+    public Object printEmployeesByDepartments() {
+        List<Employee> employees = null;
+        try {
+            employees = employeeService.printEmployeesByDepartments();
+        } catch (Throwable e) {
+            return e.getMessage();
+        }
+        return employees;
+    }
+
+    @GetMapping(path = "/print")
+    public Object printEmployees() {
+        List<Employee> employees = null;
+        try {
+            employees = employeeService.printEmployees();
+        } catch (Throwable e) {
+            return e.getMessage();
+        }
+        return employees;
+    }
 }
